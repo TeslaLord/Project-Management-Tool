a6d538ee85e8a6db320b287774d532b8ff06603c
TeslaLord <ntharun@gmail.com>
2023-04-02 22:27:37 -0400

    ntru security

--- security/security_service.py
+++ security/security_service.py
@@ -7,6 +7,97 @@ from datetime import datetime, timedelta
 import psycopg2
 import requests
 import os
+from docopt import docopt
+from ntru.ntrucipher import NtruCipher
+from ntru.mathutils import random_poly
+from sympy.abc import x
+from sympy import ZZ, Poly
+from padding.padding import *
+import numpy as np
+import sys
+import logging
+import math
+
+log = logging.getLogger("ntru")
+
+debug = False
+verbose = False
+
+
+def generate(N, p, q, priv_key_file, pub_key_file):
+    ntru = NtruCipher(N, p, q)
+    ntru.generate_random_keys()
+    h = np.array(ntru.h_poly.all_coeffs()[::-1])
+    f, f_p = ntru.f_poly.all_coeffs()[::-1], ntru.f_p_poly.all_coeffs()[::-1]
+    np.savez_compressed(priv_key_file, N=N, p=p, q=q, f=f, f_p=f_p)
+    log.info("Private key saved to {} file".format(priv_key_file))
+    np.savez_compressed(pub_key_file, N=N, p=p, q=q, h=h)
+    log.info("Public key saved to {} file".format(pub_key_file))
+
+
+def encrypt(pub_key_file, input_arr, bin_output=False, block=False):
+    pub_key = np.load(pub_key_file, allow_pickle=True)
+    ntru = NtruCipher(int(pub_key['N']), int(pub_key['p']), int(pub_key['q']))
+    ntru.h_poly = Poly(pub_key['h'].astype(int)[::-1], x).set_domain(ZZ)
+    if not block:
+        if ntru.N < len(input_arr):
+            raise Exception("Input is too large for current N")
+        output = (ntru.encrypt(Poly(input_arr[::-1], x).set_domain(ZZ),
+                               random_poly(ntru.N, int(math.sqrt(ntru.q)))).all_coeffs()[::-1])
+    else:
+        input_arr = padding_encode(input_arr, ntru.N)
+        input_arr = input_arr.reshape((-1, ntru.N))
+        output = np.array([])
+        block_count = input_arr.shape[0]
+        for i, b in enumerate(input_arr, start=1):
+            log.info("Processing block {} out of {}".format(i, block_count))
+            next_output = (ntru.encrypt(Poly(b[::-1], x).set_domain(ZZ),
+                                        random_poly(ntru.N, int(math.sqrt(ntru.q)))).all_coeffs()[::-1])
+            if len(next_output) < ntru.N:
+                next_output = np.pad(next_output, (0, ntru.N - len(next_output)), 'constant')
+            output = np.concatenate((output, next_output))
+
+    if bin_output:
+        k = int(math.log2(ntru.q))
+        output = [[0 if c == '0' else 1 for c in np.binary_repr(n, width=k)] for n in output]
+    return np.array(output).flatten()
+
+
+def decrypt(priv_key_file, input_arr, bin_input=False, block=False):
+    priv_key = np.load(priv_key_file, allow_pickle=True)
+    ntru = NtruCipher(int(priv_key['N']), int(priv_key['p']), int(priv_key['q']))
+    ntru.f_poly = Poly(priv_key['f'].astype(int)[::-1], x).set_domain(ZZ)
+    ntru.f_p_poly = Poly(priv_key['f_p'].astype(int)[::-1], x).set_domain(ZZ)
+
+    if bin_input:
+        k = int(math.log2(ntru.q))
+        pad = k - len(input_arr) % k
+        if pad == k:
+            pad = 0
+        input_arr = np.array([int("".join(n.astype(str)), 2) for n in
+                              np.pad(np.array(input_arr), (0, pad), 'constant').reshape((-1, k))])
+    if not block:
+        if ntru.N < len(input_arr):
+            raise Exception("Input is too large for current N")
+        log.info("POLYNOMIAL DEGREE: {}".format(max(0, len(input_arr) - 1)))
+        return ntru.decrypt(Poly(input_arr[::-1], x).set_domain(ZZ)).all_coeffs()[::-1]
+
+    input_arr = input_arr.reshape((-1, ntru.N))
+    output = np.array([])
+    block_count = input_arr.shape[0]
+    for i, b in enumerate(input_arr, start=1):
+        log.info("Processing block {} out of {}".format(i, block_count))
+        next_output = ntru.decrypt(Poly(b[::-1], x).set_domain(ZZ)).all_coeffs()[::-1]
+        if len(next_output) < ntru.N:
+            next_output = np.pad(next_output, (0, ntru.N - len(next_output)), 'constant')
+        output = np.concatenate((output, next_output))
+    return padding_decode(output, ntru.N)
+
+
+# print(enc_text)
+# output = decrypt("myKey.priv.npz", enc_text)
+# print(np.packbits(np.array(output).astype(int)).tobytes().decode('utf-8'))
+
 
 app = FastAPI()
 security = HTTPBasic()
@@ -50,10 +141,27 @@ async def db_connection_middleware(request: Request, call_next):
     return response
 
 def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
+    # print(hashed_password)
+    hashed_password = np.fromstring(hashed_password.strip('[]'), sep=' ')
+    output = decrypt("myKey.priv.npz", hashed_password)
+    output = np.packbits(np.array(output).astype(int)).tobytes().decode('utf-8')
+    if output == plain_password:
+        return True
+    return False
+    return True
+
+    # if get_password_hash(plain_password) == hashed_password:
+    #     return True
+    # # return False
+    # return pwd_context.verify(plain_password, hashed_password)
 
 def get_password_hash(password):
-    return pwd_context.hash(password)
+    input_arr = np.unpackbits(np.frombuffer(password.encode('utf-8'), dtype=np.uint8))
+    input_arr = np.trim_zeros(input_arr, 'b')
+    output = encrypt("myKey.pub.npz", input_arr)
+    # output = np.array2string(output)
+    # output = pwd_context.hash(output)
+    return np.array2string(output)
 
 def get_user(request, username):
     cur = request.state.db.cursor()
@@ -65,7 +173,6 @@ def get_user(request, username):
 
 def authenticate_user(request, username, password):
     user = get_user(request, username)
-    print(user)
     if not user:
         return False
     if not verify_password(password, user["enc_psd"]):
@@ -106,7 +213,7 @@ async def get_current_active_user(current_user=Depends(get_current_user)):
 
 ##
 @app.post('/token')
-async def login_for_access_token(request: Request, form_data: OAuth2PasswordRequestForm= Depends()):
+async def login_for_access_token(request: Request, form_data: OAuth2PasswordRequestForm= Depends(   )):
     user = authenticate_user(request, form_data.username, form_data.password)
     if not user:
         raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED, detail="Incorrect Credentials", headers = {"WWW-Authenticate": "Bearer"})
@@ -222,3 +329,6 @@ async def get_employee_id( employee_name, current_user = Depends(get_current_act
         'employee_name': employee_name
     }
     return make_get_request(url, params)
+
+
+
